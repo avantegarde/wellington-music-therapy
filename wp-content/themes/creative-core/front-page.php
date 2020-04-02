294fe919f7ad31105a8832e819902097fc1e6f5d
kaelsteinert <kaelsteinert@gmail.com>
2020-04-02 13:02:29 -0400

    updated wp and plugins. theme tweaks

--- wp-content/themes/creative-core/front-page.php
+++ wp-content/themes/creative-core/front-page.php
@@ -18,30 +18,23 @@ get_header(); ?>
             <div class="home_slider">
             <div class="" style="background-image:url('/wp-content/themes/creative-core/inc/images/home-slide-01.jpg');">
                     <div class="slide-caption right line-box">
-                        <h3 class="section-title">Quality, Professionalism<br>and Passion.</h3>
-                        <p class="subline">Our staff strives to exceed your expectations<br> by making Stitch by Stitch a fun place to shop!  </p>
-                        <a href="/shop/" data-button="arrow">Online Shopping Coming Soon</a>
+                        <h3 class="section-title">Home Page Slide #1 Title<br>Goes Here.</h3>
+                        <p class="subline">Nihil molestiae consequatur, vel illum qui dolorem eum. Qui officia deserunt mollit?</p>
+                        <a href="#" data-button="arrow">Learn More</a>
                     </div>
                 </div>
                 <div class="" style="background-image:url('/wp-content/themes/creative-core/inc/images/home-slide-02.jpg');">
                     <div class="slide-caption line-box">
-                        <h3 class="section-title">Educate, Support<br>& Encourage</h3>
-                        <p class="subline">Register for our classes,<br> taught by professionals and accompanied by fun!</p>
-                        
-                        <a href="/category/classes/" data-button="arrow">Get The Details</a>
+                        <h3 class="section-title">Home Page Slide #2 Title<br>Goes Here.</h3>
+                        <p class="subline">Nihil molestiae consequatur, vel illum qui dolorem eum. Qui officia deserunt mollit?</p>
+                        <a href="#" data-button="arrow">Learn More</a>
                     </div>
                 </div>
-                <div class="" style="background-image:url('/wp-content/themes/creative-core/inc/images/home-slide-01.jpg');">
-                    <div class="slide-caption center">
-                        <h3 class="section-title">Stitch by Stitch</h3>
-                        <p class="subline">We have over 30 years of experience in the business of quilting and sewing backed by over 60 years of quilting and sewing experience.</p>
-                        <a href="/about/" data-button="arrow">Get To Know Us</a>
-                    </div>
-                </div>
-                <div class="" style="background-image:url('/wp-content/themes/creative-core/inc/images/home-slide-02.jpg');">
-                    <div class="slide-caption right line-box">
-                    	<h3 class="section-title">Great Pricing!</h3>
-                        <p class="subline">We discount all our sewing, quilting and embroidery machines and well as our sergers. If you purchase a machine from us and decide within the first year that you wish to upgrade to another model we will apply the purchase price of your machine to the MSRP of the next model.</p>
+                <div class="" style="background-image:url('/wp-content/themes/creative-core/inc/images/home-slide-03.jpg');">
+                    <div class="slide-caption center line-box">
+                        <h3 class="section-title">Home Page Slide #3 Title<br>Goes Here.</h3>
+                        <p class="subline">Nihil molestiae consequatur, vel illum qui dolorem eum. Qui officia deserunt mollit?</p>
+                        <a href="#" data-button="arrow">Learn More</a>
                     </div>
                 </div>
             </div>
@@ -49,140 +42,100 @@ get_header(); ?>
 
         <section id="home-services" class="green services">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="panel center" data-col="homeService">
-                            <span class="icon">
-                                <i class="fas fa-flag-checkered"></i>
-                            </span>
-                            <h3 class="title">Unique & Modern</h3>
-                            <p>Stitch by Stitch is one of the largest quilting and sewing stores in the Kingston area, offering a truly unique shopping destination. We are your go-to resource for all your sewing and quilting needs, carrying the most current fabric designs.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="panel center" data-col="homeService">
+                <h3 class="section-title block-title center" data-color="white">Our Services</h3>
+                <div class="row services-flex">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
                             <span class="icon">
                                 <i class="fas fa-user"></i>
                             </span>
-                            <h3 class="title">Superb Service</h3>
-                            <p>Our staff always looks forward to sharing their knowledge and skills with anyone who walks through our doors, phones or emails us. We take the time to help support, encourage and inspire you with any challenge you come across.</p>
+                            <h3 class="title">Individual Music Therapy</h3>
+                            <p>Together we will set goals and objectives for our sessions. Music will be used as the catalyst to promote growth and reach these goals.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="panel center" data-col="homeService">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
                             <span class="icon">
-                                <i class="fas fa-dollar-sign"></i>
+                                <i class="fas fa-user"></i>
                             </span>
-                            <h3 class="title">Competitive Pricing</h3>
-                            <p>Our fabric is sold using metric measurements not Imperial however, we are able to converse in and convert both. We are careful to make sure that our prices are fair and competitive.</p>
+                            <h3 class="title">Adapted Music Lessons</h3>
+                            <p>Modifications are made to music lessons to create a learning environment suitable to the student's needs and learning pace.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
-
-        <section id="sale-section" class="home-sales">
-            <div class="container">
-                <h3 class="section-title center">Shop Online</h3>
-
-                <div class="tabs-wrap">
-                    <ul class="tabs-menu">
-                        <li class="active current"><a data-toggle="tab" href="#tab-1">Tab #1</a></li>
-                        <li><a data-toggle="tab" href="#tab-2">Tab #2</a></li>
-                        <li><a data-toggle="tab" href="#tab-3">Tab #3</a></li>
-                    </ul>
-                    <div class="tab-content">
-                        <div id="tab-1">
-                            <h3>Tab Content #1</h3>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.</p>
-                            <ul>
-                                <li>Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</li>
-                                <li>Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia.</li>
-                                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</li>
-                            </ul>
-                            <p>Qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit. Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
-                            <p>Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam. Ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
-                        </div>
-                        <div id="tab-2">
-                            <h3>Tab Content #2</h3>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.</p>
-                            <ul>
-                                <li>Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</li>
-                                <li>Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia.</li>
-                                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</li>
-                            </ul>
-                            <p>Qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit. Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
-                            <p>Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam. Ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
-                        </div>
-                        <div id="tab-3">
-                            <h3>Tab Content #3</h3>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.</p>
-                            <ul>
-                                <li>Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</li>
-                                <li>Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia.</li>
-                                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</li>
-                            </ul>
-                            <p>Qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit. Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
-                            <p>Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam. Ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
+                            <span class="icon">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            <h3 class="title">Sing it Girls</h3>
+                            <p>We are the Guelph & Area providers of the unique and exciting Sing it girls Vocal program for girls aged 7-12.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
                         </div>
                     </div>
-                </div>
 
-
-
-
-
-               <p>Our website will offer online sales in the future. Keep checking back for updates as we continue to grow.</p>
-              <!---<?php echo do_shortcode('[products limit="4" columns="4" category="" order="rand" on_sale=”true”]'); ?>--->
-            </div>
-        </section>
-
-   <section id="shop-categories" class="shop-cat-grid">
-            <div class="container">
-                <h3 class="section-title block-title center">Shop By Category</h3>
-                <div class="row">
-                	
-                    <?php echo do_shortcode('[product-cats include="20,18,17,19,21]'); ?>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
+                            <span class="icon">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            <h3 class="title">Long Term Care & Retirement</h3>
+                            <p>Participants can experience the benefits of social interaction and brain stimulation while in a group setting, listening to a variety of music.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
+                            <span class="icon">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            <h3 class="title">TiTi Tots</h3>
+                            <p>Music is proven to improve cognitive development and creativity. Jump start your tot's love of music with these fun, lively group sessions.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="panel center">
+                            <span class="icon">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            <h3 class="title">Community Music Programs</h3>
+                            <p>Through group participation, we strive to create  musical collaboration that include singing, songwriting and performances.</p>
+                            <p class="bottom"><a href="/about/" data-button>Learn More</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
 
         <section id="home-services" class="blank">
-            <div class="parallax auto-height" data-plx-img="/wp-content/uploads/2018/04/Homepage-Family-Owned-and-Operated.jpg">
-                 <div class="container">
-                    <div class="col-md-7 line-box float-right" data-col="hp-bottom-cta">
-                       
-                            <h2 class="section-title" data-color="white">Family Owned <br>and Operated</h2>
-                            <p>Margaret inherited her sewing skills from her maternal grandmother who taught her to sew when she was just 6 years old.  Margaret has sewn off and on her entire life while she worked for a Canadian Bank for over twenty years.  After moving to the Kingston area with her three children in 1986 she decided a career change was in order.  She began working for a specialty fabric store in the late 80’s and branched out on her own in the mid 90’s.  Her first shop was called The Perfect Stitch. </p>
-                            <p class="txt-right"><a href="/about/" data-button>Learn More</a></p>
-                    
+            <div class="parallax auto-height" data-plx-img="/wp-content/themes/creative-core/inc/images/hero.jpg">
+                <div class="container">
+                    <div class="col-md-8 col-md-offset-2 line-box center" data-col="hp-bottom-cta">
+                        <h2 class="section-title" data-color="white">Architecto beatae vitae dicta</h2>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Eaque ipsa quae ab illo inventore veritatis et quasi. Architecto beatae vitae dicta sunt explicabo. Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia quia voluptas sit.</p>
+                        <p><a href="/about/" data-button>Learn More</a></p>
                     </div>
                 </div>
             </div>
         </section>
 
-         <section id="home-services" class="blank">
-            <div class="parallax auto-height" data-plx-img="/wp-content/uploads/2018/04/does-your-machine-need-servicing.jpg">
+        <!-- <section id="home-services" class="blank">
+            <div class="parallax auto-height" data-plx-img="/wp-content/themes/creative-core/inc/images/hero.jpg">
                 <div class="container">
                     <div class="col-md-7 line-box" data-col="hp-bottom-cta">
-                       
-                             <h3 class="machine-title" data-color="white">Does your <span>machine</span> need <strong>servicing?</strong></h3>
+                        <h3 class="machine-title" data-color="white">Does your <span>machine</span> need <strong>servicing?</strong></h3>
                         <p>The investment that you make in a sewing machine is as important to you as it is us at Stitch by Stitch.</p>  
-						<p>We want you to always get the best performance you can from your machine.  To ensure that it continually works to your satisfaction, you will find that cleaning it on a regular basis will help to keep your machine running smoothly!
-						</p>
+                        <p>We want you to always get the best performance you can from your machine.  To ensure that it continually works to your satisfaction, you will find that cleaning it on a regular basis will help to keep your machine running smoothly!</p>
                         <p class="txt-right"><a href="/services/" data-button>Let Us Help</a></p>
-                    
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
-       
-
-          
-                       
-         <section id="home-services" class="blank">
-            <div class="parallax auto-height" data-plx-img="/wp-content/uploads/2018/04/homepage-Want-to-learn.jpg">
+        <!-- <section id="home-services" class="blank">
+            <div class="parallax auto-height" data-plx-img="/wp-content/themes/creative-core/inc/images/hero.jpg">
                 <div class="container">
                     <div class="col-md-7 line-box float-right" data-col="hp-bottom-cta">
 
@@ -194,11 +147,11 @@ get_header(); ?>
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
-        <?php while (have_posts()) : the_post(); ?>
-            <?php echo the_content(); ?>
-        <?php endwhile; ?>
+        <?php // while (have_posts()) : the_post(); ?>
+            <?php // echo the_content(); ?>
+        <?php // endwhile; ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
